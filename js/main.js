e323f98ef0281fe076b982b7ac1905196f9dcad5
natsu02mi <summer.ntmg@gmail.com>
2022-09-02 07:07:16 +0900

    提出用

--- js/main.js
+++ js/main.js
@@ -22,6 +22,32 @@ $(function($){
         centerMode: true,
         centerPadding: '20%',
     });
+
+    $('.more-button').on('click', function(){
+        if ($('.open-box').is(':hidden')){
+            $('.open-box').slideDown();
+            $(this).text('close');
+            $(this).css('background-color', '#808080');
+        } else {
+            $('.open-box').slideUp();
+            $(this).text('more');
+            $(this).css('background-color', '#fabb51');
+        }
+    });
+    
+    $(".modal-button").modaal();
+
+    $('nav a[href^="#"]').click(function(){
+        var adjust = 0;
+        var speed = 400;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - adjust;
+        $('body,html').animate({
+            scrollTop: position
+        },speed, 'swing');
+        return false;
+    });
 });
 
 
